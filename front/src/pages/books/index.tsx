c8cf7f9d97b46a9617b553f0761924491cf69629
YoshimasaAsada <yoshimasa111112@gmail.com>
2023-04-29 17:00:49 +0900

    add: Book一覧の取得機能の追加

--- front/src/pages/books/index.tsx
+++ front/src/pages/books/index.tsx
@@ -1,9 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Index() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/books").then((res) => {
+      setBooks(res.data);
+    });
+  }, []);
+
   return (
     <>
       <div>
         <h1>Books/index</h1>
+        {books.map((book: Book, index) => {
+          return (
+            <div key={index}>
+              {/* 一番上の要素にkeyを入れる */}
+              {book.title}
+              {book.body}
+            </div>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
